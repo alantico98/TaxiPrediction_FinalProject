cc30dca6b63440779a78c866817fda9b672544dc
alantico98 <alantico98@gmail.com>
2025-09-02 01:21:14 -0600

    Modify evaluate and test files for api and remove compose and Makefile

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,5 @@
 import json
 import requests
-from sklearn.metrics import accuracy_score
 
 API_URL = "http://localhost:8000/predict"  # replace with your EC2 URL
 TEST_DATA_FILE = "test.json"
@@ -11,27 +10,23 @@ def main():
     with open(TEST_DATA_FILE, "r") as f:
         test_data = json.load(f)
 
-    y_true = []
     y_pred = []
 
     for entry in test_data:
         input_data = {
-            "text": entry["text"],
-            "true_label": entry["true_label"]
+            "PULocationID": entry["PULocationID"],
+            "DOLocationID": entry["DOLocationID"],
+            "timestamp": entry["timestamp"]
         }
 
         try:
             response = requests.post(API_URL, json=input_data)
             response.raise_for_status()
-            prediction = response.json()["sentiment"]
-            y_true.append(entry["true_label"].lower())
+            prediction = response.json()["prediction"]
             y_pred.append(prediction.lower())
         except Exception as e:
             print(f"Error making prediction for {entry['text']}: {e}")
             continue
-
-    acc = accuracy_score(y_true, y_pred)
-    print(f"Model Accuracy: {acc:.2f}")
 
 
 if __name__ == "__main__":
